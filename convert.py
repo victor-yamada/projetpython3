6131134cf2b557f670f132ab7a362a0f4444e5b7
victor-yamada <150898679+victor-yamada@users.noreply.github.com>
2023-12-10 18:17:21 +0000

    Add files via upload
    Voici le projet de l'application Smart_convert. quelques modifications ont été apportées au projet, et désormais l'application prend en compte toutes les devises du monde entier; il suffit juste de connaitre le code de la devise et le tour est jouer!
    l'utilisateur a besoin d'être connecter à internet pour pouvoir effectuer une conversion quelconque. De plus une fonction de retour à l'opération précédente à été ajoutée pour permettre aux utilisateurs de voir des opération ulterieures s'ils le désirent, néeanmoins cette fonction n'a pas pu être complètement implémenter en raison du temps imparti et des conditions de santé du concepteur.
    Le logiciel rempli les exigences de fonctionnement.
    Que vos conversion soient désormais comme de l'eau à boire pour vous!

--- convert.py
+++ convert.py
@@ -2,6 +2,7 @@ from tkinter import *
 import requests
 import sys
 from PIL import Image, ImageTk
+import sqlite3
 
 
 def get_exchange_rate(from_currency, to_currency):
@@ -14,36 +15,85 @@ def update_result_label(text):
 
 def convert_currency():
     try:
-        amount = float(entry_amount.get())
+        amount = int(entry_amount.get())
         from_currency = entry_from_currency.get().upper()
         to_currency = entry_to_currency.get().upper()
         
         exchange_rate = get_exchange_rate(from_currency, to_currency)
         result = amount * exchange_rate
         
+        # Enregistrer l'opération dans la base de données
+        cursor.execute(f"INSERT INTO operations (amount, from_currency, to_currency, result) VALUES ({amount}, '{from_currency}', '{to_currency}', {result})")
+        conn.commit()
+        
+
         # Affiche le résultat dans la fenêtre tkinter
-        update_result_label(f"{amount} {from_currency} =  {result} {to_currency}")
+        update_result_label(f"{amount} {from_currency} = {result} {to_currency}")
     
     except ValueError:
         # Affiche le message d'erreur dans la fenêtre tkinter
         update_result_label("Erreur! Montant non valide; Veuillez entrer un nombre correcte.")
 
+def get_last_operation():
+    try:
+        cursor.execute("SELECT * FROM précédent ORDER BY id DESC LIMIT 1")
+        last_operation = cursor.fetchone()
+
+        entry_amount.delete(0, END)
+        entry_amount.insert(0, last_operation[1])
+
+        entry_from_currency.delete(0, END)
+        entry_from_currency.insert(0, last_operation[2])
+
+        entry_to_currency.delete(0, END)
+        entry_to_currency.insert(0, last_operation[3])
+
+        update_result_label(f"{last_operation[1]} {last_operation[2]} = {last_operation[4]} {last_operation[3]}")
+    
+    except sqlite3.Error:
+        update_result_label("Erreur! Aucune opération précédente n'a été effectuée.")
 def reset_fields():
-    entry_amount.delete(0, END)
+    entry_amount.delete(0, END) 
     entry_from_currency.delete(0, END)
     entry_to_currency.delete(0, END)
     update_result_label("")
 
+# Connexion à la database
+conn = sqlite3.connect('convertisseur.db')
+cursor = conn.cursor()
 
+# Création de la table "précédent" 
+cursor.execute("""
+CREATE TABLE if not exists précédent (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    amount REAL,
+    from_currency TEXT,
+    to_currency TEXT,
+    result REAL
+)
+""")
 
+# Création de la table "précédent" 
+cursor.execute("""
+CREATE TABLE if not exists précédent (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    amount REAL,
+    from_currency TEXT,
+    to_currency TEXT,
+    result REAL
+)
+""")
 
 app = Tk()
 app.configure(bg="Lightslategray")
+app.geometry("700x220")
 app.title("Smart_convert")
+
 
 # Restreindre la taille de la fenêtre
 app.resizable(False, False)
 
+# Ajout et Configuration de l'image
 image = Image.open("C:\\Users\\admin\\Documents\\Cours Python3\\pythonProject convertisseur\\plume.jpg")
 image = image.resize((int(image.width / 13), int(image.height / 13)))
 photo = ImageTk.PhotoImage(image)
@@ -52,34 +102,52 @@ photo = ImageTk.PhotoImage(image)
 image_label = Label(app, image=photo, bg="light blue")
 image_label.grid(row=0, column=2, rowspan=3, padx=5, pady=5)
 
-
+# Créer un label pour entrer le montant
 entry_amount = Entry(app, width=20)
 entry_amount.grid(row=0, column=1)
 
-amount_label = Label(app, text="Entrez le montant que vous souhaitez convertir")
+# Créer un label pour le texte "Entrez le montant que vous souhaitez convertir"
+amount_label = Label(app, text="Entrez le montant que vous souhaitez convertir", bg="Lightslategray")
 amount_label.grid(row=0, column=0, padx=10, pady=10)
 
+# Créer un label pour entrer la devise actuelle
 entry_from_currency = Entry(app, width=20)
 entry_from_currency.grid(row=1, column=1,padx=10, pady=10)
 
-
-from_currency_label = Label(app, text="Entrez la devise actuelle")
+# Créer un label texte "entrer la devise actuelle"
+from_currency_label = Label(app, text="Entrez la devise connue", bg="Lightslategray")
 from_currency_label.grid(row=1, column=0,padx=10, pady=10)
 
-
+# Créer un label pour entrer la devise voulu
 entry_to_currency = Entry(app, width=20)
 entry_to_currency.grid(row=2, column=1)
 
-to_currency_label = Label(app, text="Entrez la devise voulu")
+# Créer un label texte "entrer la devise voulu"
+to_currency_label = Label(app, text="Entrez la devise voulu", bg="Lightslategray")
 to_currency_label.grid(row=2, column=0,padx=10, pady=10)
 
+# Créer le bouton de conversion
 convert_button = Button(app, text="Convertir", command=convert_currency, bg="green")
 convert_button.grid(row=3, column=1, padx=10, pady=10)
 
-reset_button = Button(app, text="Réinitialiser", command=reset_fields, bg="Crimson")
+# Création du bouton de réinitialisation de l'opération
+reset_button = Button(app, text="Réinitialiser", command=reset_fields, bg="Indianred")
 reset_button.grid(row=3, column=0,)
 
-result_label = Label(app, text="")
+# message
+amount_label = Label(app, text="Exemple code de devise: ",font=("arial", 10), fg="Navajowhite", bg="Lightslategray")
+amount_label.grid(row=4, column=0)
+
+amount_label = Label(app, text=" 'USD' 'EUR' 'GBP' 'CAD' 'XAF' ",font=("arial", 10), fg="Navajowhite", bg="Lightslategray")
+amount_label.grid(row=4, column=1)
+
+# Création du bouton pour revenir à la précédente opération
+reset_button = Button(app, text="Précédent", command=get_last_operation, bg="Navajowhite")
+reset_button.grid(row=4, column=2)
+
+#Création du label d'affichage du résultat
+result_label = Label(app, text="", bg="Lightslategray")
 result_label.grid(row=3, column=2, padx=10)
 
 app.mainloop()
+conn.close()
